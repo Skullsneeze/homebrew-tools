b7cf1881fe4ffe7b2e14a8a3370370204eb666e6
Martijn Swinkels <martijn.swinkels@wearejust.com>
2025-06-11 07:38:24 +0200

    Update SSHop tap

--- sshop.rb
+++ sshop.rb
@@ -1,7 +1,7 @@
 class Sshop < Formula
   desc "Simple SSH jump tool for managing and connecting to servers"
   homepage "https://github.com/Skullsneeze/sshop"
-  url "https://github.com/Skullsneeze/sshop/releases/download/1.0.1/sshop"
+  url "https://github.com/Skullsneeze/sshop/releases/download/v1.0.1/sshop"
   sha256 "b2ac17df02f4bb8e30992c7c5ee7284c0cce29836820e43966e40fcb82d8012a"
   version "1.0.1"
 
